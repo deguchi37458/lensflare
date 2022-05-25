7f97b1231b8cf63ff634e49b00456290563e5587
deguchi37458 <deguchi37458@gmail.com>
2022-05-26 07:52:07 +0900

    add FlyControls

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-import * as THREE from "./build/three.module.js"
+import * as THREE from "./build/three.module.js";
+import {FlyControls} from "./jsm/controls/FlyControls.js";
 
 let camera, scene, renderer;
+let controls;
+
+const clock = new THREE.Clock();
 
 init();
 
@@ -59,6 +63,20 @@ function init(){
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+    
+    //マウス操作
+    controls = new FlyControls(camera, renderer.domElement);
+    
+    controls.movementSpeed = 2500;
+    controls.rollSpeed = Math.PI / 20;
 
+    animate();
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    const delta = clock.getDelta(); //経過した時間を取得
+    controls.update(delta);
     renderer.render(scene, camera);
 }
